afe2236a0ccbe131eb8b298a8acfe543bcbe54c3
Rolph Recto <rolph.recto@gmail.com>
2024-06-04 02:27:01 -0400

    fixed cmdline

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
 use clap::Parser;
 use serde::Serialize;
 use serde_json::Value;
-use std::{io::{self, Read}, fs};
+use std::{
+    io::{self, Read},
+    fs,
+    path::Path
+};
 
 use serde_datalog::DatalogExtractor;
 
+#[derive(Parser, Debug)]
+#[command(
+    version = "0.1.0",
+    about,
+    long_about = Some("Converts input in a variety of formats to a database of facts.")
+)]
+struct Args {
+    #[arg(
+        index = 1,
+        help = "(Optional) Input file; if absent, will read from standard input"
+    )]
+    filename: Option<String>,
+
+    #[arg(short = 'o', long = "output", help = "File name of output SQLite database")]
+    output: String,
+}
+
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
+    let args = Args::parse();
+
+    let mut input: String = String::new();
+    match args.filename {
+        Some(filename) => {
+            input = fs::read_to_string(&filename).unwrap()
+
+        },
+        None => {
+            io::stdin().read_to_string(&mut input).unwrap();
+        }
+    };
+
     let value: Value = serde_json::from_str(&input).unwrap();
     let mut extractor = DatalogExtractor::new();
     value.serialize(&mut extractor).unwrap();
-    let _ = fs::remove_file("json2.db");
-    extractor.dump();
-    extractor.dump_to_db("json2.db").unwrap();
+
+    let outpath = Path::new(&args.output);
+    if outpath.is_file() {
+        fs::remove_file(&args.output).unwrap();
+    }
+
+    extractor.dump_to_db(&args.output).unwrap();
 }
