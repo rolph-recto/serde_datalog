9b7d0a3f13038c81d3594324df320f92d459ab21
Rolph Recto <rolph.recto@gmail.com>
2024-06-14 01:05:42 -0400

    more value tests

--- tests/test_values.rs
+++ tests/test_values.rs
@@ -12,7 +12,7 @@ fn get_backend(value: Value) -> backend::vector::Backend {
 }
 
 #[test]
-fn run_values() {
+fn run_value1() {
     let value: Value = 
         serde_json::Map::from_iter(
             vec![
@@ -32,4 +32,58 @@ fn run_values() {
     
     assert!(backend.string_table.first().unwrap().0 == map_key);
     assert!(backend.number_table.first().unwrap().0 == map_value);
+}
+
+#[test]
+fn run_value2() {
+    let value: Value = 
+        Value::Array(vec![
+            Value::String("a".to_string()),
+            Value::String("b".to_string()),
+        ]);
+
+    let backend = get_backend(value);
+
+    assert!(backend.seq_table.len() == 2);
+    assert!(backend.string_table.len() == 2);
+    assert!(backend.type_table.len() == 3);
+
+    let a_sym =
+        backend.symbol_table.iter()
+        .find_map(|(s, id)|
+            if s == "a" { Some(*id) } else { None }
+        ).unwrap();
+
+    let b_sym =
+        backend.symbol_table.iter()
+        .find_map(|(s, id)|
+            if s == "b" { Some(*id) } else { None }
+        ).unwrap();
+
+    let a_id = 
+        backend.string_table.iter()
+        .find_map(|(id, sym)|
+            if *sym == a_sym { Some(*id) } else { None }
+        ).unwrap();
+
+    let b_id = 
+        backend.string_table.iter()
+        .find_map(|(id, sym)|
+            if *sym == b_sym { Some(*id) } else { None }
+        ).unwrap();
+
+    let seq_first_elem = 
+        backend.seq_table.iter()
+        .find_map(|(_, index, val)|
+            if *index == 0 { Some(*val) } else { None }
+        ).unwrap();
+
+    let seq_second_elem = 
+        backend.seq_table.iter()
+        .find_map(|(_, index, val)|
+            if *index == 1 { Some(*val) } else { None }
+        ).unwrap();
+
+    assert!(seq_first_elem == a_id);
+    assert!(seq_second_elem == b_id);
 }
